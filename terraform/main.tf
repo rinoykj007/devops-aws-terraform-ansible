5cab088a3c77ebcc205aa5e671060266981b4edd
Rinoy joseph <rinoykj007@example.com>
2025-03-13 11:09:41 +0000

    Update Terraform config to use data source for security group and add key_name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,19 @@
-﻿provider "aws" {
+provider "aws" {
   region = "eu-west-1"
 }
 
-# Use existing security group
-resource "aws_security_group" "web_server" {
-  name        = "web_server"
-  description = "Allow SSH and HTTP traffic"
-
-  ingress {
-    description = "SSH from anywhere"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP from anywhere"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "web_server"
-  }
-
-  # Prevent recreation of existing security group
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes = [
-      description,
-      tags
-    ]
-  }
+# Import existing security group
+data "aws_security_group" "web_server" {
+  id = "sg-0f108ebec9a19226a"
 }
 
 # Use existing EC2 instance
 resource "aws_instance" "web_server" {
   ami           = "ami-0694d931cee176e7d"  # Amazon Linux 2 AMI in eu-west-1
   instance_type = "t3.micro"
+  key_name      = "deployer-key-new"
 
-  vpc_security_group_ids = [aws_security_group.web_server.id]
+  vpc_security_group_ids = [data.aws_security_group.web_server.id]
 
   tags = {
     Name = "DevOps_Ca1"
