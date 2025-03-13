1c83ebc4dac422a10a387a40d1ad0fc5ce895ad9
Rinoy joseph <rinoykj007@example.com>
2025-03-13 10:03:55 +0000

    Remove inventory.ini and import scripts

--- terraform/import.ps1
+++
@@ -1,6 +0,0 @@
-# Import existing security group
-terraform import aws_security_group.web_server sg-0f108ebec9a19226a
-
-# Import existing EC2 instance
-$instanceId = aws ec2 describe-instances --filters "Name=tag:Name,Values=DevOps_Ca1" --query "Reservations[0].Instances[0].InstanceId" --output text
-terraform import aws_instance.web_server $instanceId
--- terraform/import.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Import existing security group
-terraform import aws_security_group.web_server sg-0f108ebec9a19226a
-
-# Import existing EC2 instance
-terraform import aws_instance.web_server $(aws ec2 describe-instances --filters "Name=tag:Name,Values=DevOps_Ca1" --query "Reservations[0].Instances[0].InstanceId" --output text)
